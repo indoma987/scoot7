76ca144064889a3711b01dba0989fefed774622b
indoma987 <161834677+indoma987@users.noreply.github.com>
2024-10-08 10:43:27 -0300

    hmnh

---
+++ main.js
@@ -0,0 +1,12 @@
+function mostraResposta (cartao_conteudo){
+    const resposta = cartao_conteudo.querySelector('.cartao_conteudo_resposta');
+    if (resposta.style.display =='none'){
+        resposta.style.display == 'block';
+    }
+
+
+else{
+    resposta.style.display == 'none';
+}
+
+}
