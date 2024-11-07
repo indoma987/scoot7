da1707da3b4d7e1fb789388e60863262ddb0299a
indoma987 <161834677+indoma987@users.noreply.github.com>
2024-11-07 10:39:10 -0300

    p-0

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-function mostraResposta (cartao_conteudo){
-    const resposta = cartao_conteudo.querySelector('.cartao_conteudo_resposta');
-    if (resposta.style.display =='none'){
-        resposta.style.display == 'block';
+function mostraResposta (card){
+    const resposta = card.querySelector('.cartao__conteudo__resposta');
+    if (resposta.style.display ==='none'){
+        resposta.style.display = 'block';
     }
 
 
 else{
-    resposta.style.display == 'none';
+    resposta.style.display = 'none';
 }
 
 }
